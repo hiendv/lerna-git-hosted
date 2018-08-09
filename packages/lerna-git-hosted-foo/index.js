e4a3ab6c68efbef376b2f45e131d89d978927200
hiendv <hien.dv.neo@gmail.com>
2018-08-09 15:07:52 +0700

    lerna-git-hosted-foo: add snake method (minor change)

--- packages/lerna-git-hosted-foo/index.js
+++ packages/lerna-git-hosted-foo/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+var snakeCase = require('lodash.snakecase');
 var main = {
   up (str) {
     return str.toUpperCase()
   },
   low (str) {
     return str.toLowerCase()
+  },
+  snake (str) {
+    return snakeCase(str)
   }
 };
 
